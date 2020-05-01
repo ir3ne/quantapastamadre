769ff957f8d102ecfebc041407b416c8a16648a0
irene <i.tomaini@tuotempo.com>
2020-05-01 17:48:00 +0200

    main style added

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PhaseTag from '../PhaseTag/PhaseTag';
 import PhaseTitle from '../PhaseTitle/PhaseTitle';
 import IngredientTitle from '../IngredientTitle/IngredientTitle';
+import Separator from '../Separator/Separator';
 
 import classNames from './Ingredients.module.scss';
 
@@ -13,24 +14,27 @@ const Ingredients = (props) => {
   return (
     <>
       <div className={classNames.madreIngredients}>
-        <PhaseTag>First Step</PhaseTag>
+        <PhaseTag>Prima fase</PhaseTag>
         <PhaseTitle>Autolisi</PhaseTitle>
-        <IngredientTitle>Flour</IngredientTitle>
-        <p>{flour} g oppure</p>
+        <IngredientTitle>Farina</IngredientTitle>
+        <p>{flour} g</p>
         <IngredientTitle>In alternativa</IngredientTitle>
         <p>
           {twoFlours} g di un tipo di farina + {twoFlours} g di un altro tipo di farina
         </p>
+        <Separator/>
         <IngredientTitle>Salt</IngredientTitle>
         <p>{salt} g</p>
+        <Separator/>
         <IngredientTitle>Water</IngredientTitle>
         <p>{water} g</p>
       </div>
       <div className={classNames.madreIngredients}>
-        <PhaseTag>Second Step</PhaseTag>
+        <PhaseTag>Seconda fase</PhaseTag>
         <PhaseTitle>Impasto</PhaseTitle>
         <IngredientTitle>Salt</IngredientTitle>
         <p>{salt} g</p>
+        <Separator/>
         <IngredientTitle>Water</IngredientTitle>
         <p>remaining water</p>
       </div>
