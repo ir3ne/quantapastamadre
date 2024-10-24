dcd64f803fbc82a4dd5b555b3ddbb30e3b7b2404
Irene Tomaini <irene.tomaini@refurbed.com>
2024-10-24 13:33:10 +0200

    fix input allowed char

--- src/common/common.js
+++ src/common/common.js
@@ -2,3 +2,5 @@ export const closeInfo = () => {
   const info = document.getElementById('info');
   info.style.display = 'none';
 }
+
+export const blockInvalidChar = e => ['e', 'E', '+', '-'].includes(e.key) && e.preventDefault();
